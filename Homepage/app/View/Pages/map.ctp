f0240020c91cac77e3c750511d2abf9c69f25a59
Martin <Martin@192.168.178.20>
2013-04-24 12:13:40 +0200

    Add defoult.css add Waypoints add Drag$drop in Map

--- Homepage/app/View/Pages/map.ctp
+++ Homepage/app/View/Pages/map.ctp
@@ -1,37 +1,35 @@
+<?php echo $this->Html->css('defult');?>
 <?php echo $this->Html->css('map');?>
 <script
-	src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&language=de"></script>
+	src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&language=de">
+</script>
 <?php echo $this->Html->script('map');?>
-<div class="container">
-	<div class="row-fluid">
-		<div class="span12">
-			<div class="row-fluid">
-				<div class="span4">
-					<div class="row-fluid">
-						<div class="span12">
-							<div id="routepanel">
-								<div class="span6">
-									<b>Start: </b><br>
-									<input id="start" type="textbox">
-								</div>
-								<div class="span6">
-									<b>End: </b><br>
-									<input id="end" type="textbox"><br>
-								</div>
-								<input type="button" value="Route" onclick="calcRoute()"><br>
-								<input id="address" type="textbox"><br>
-								<input type="button" value="Geocode" onclick="codeAddress()">
-							</div>
-						</div>
-						<div class="span12">
-							<div id="directions-panel"></div>
-						</div>
-					</div>
-				</div>
-				<div class="span8">
-					<div id="map-canvas"></div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>			
+<div id="map-canvas"></div>
+<div id="directions-panel">
+<p>Total Distance: <span id="total"></span></p>
+<div>
+	<b>Start: </b>
+	<input id="start" type="textbox"><br>
+	<b>End: </b>
+	<input id="end" type="textbox"><br>
+	<b>Zwischenstop</b><br>
+	
+	<select multiple id="waypoints" type="textbox">
+      <option value="Radolfzell">Radolfzell</input>
+      <option value="Allensbach">Allensbach</input>
+      <option value="78465 Dettingen">Dettingen</input>
+    </select>
+    <b>Mode of Travel: </b>
+    <select id="mode">
+      <option value="DRIVING">Driving</option>
+      <option value="WALKING">Walking</option>
+      <option value="BICYCLING">Bicycling</option>
+      <option value="TRANSIT">Transit</option>
+    </select>
+	<input type="button" value="Route" onclick="calcRoute()"><br>
+	<b>Suche</b><br>
+	<input id="address" type="textbox"><br>
+	<input type="button" value="Geocode" onclick="codeAddress()">
+</div>		
+</div>
+
--- Homepage/app/webroot/js/map.js
+++ Homepage/app/webroot/js/map.js
@@ -1,18 +1,18 @@
 var rendererOptions = {
 	draggable : true
 };
-var directionsDisplay;
+var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
 var directionsService = new google.maps.DirectionsService();
-var geocoder;
+var geocoder = new google.maps.Geocoder();
+var center = new google.maps.LatLng(47.6667, 9.18333);
 var map;
 
 function initialize() {
-	geocoder = new google.maps.Geocoder();
-	directionsDisplay = new google.maps.DirectionsRenderer();
+	
 	var mapOptions = {
 		zoom : 8,
 		mapTypeId : google.maps.MapTypeId.ROADMAP,
-		center : new google.maps.LatLng(47.6667, 9.18333)
+		center : center
 	};
 
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
@@ -22,21 +22,53 @@ function initialize() {
 	var control = document.getElementById('control');
 	control.style.display = 'block';
 	map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+	
+	google.maps.event.addListener(directionsDisplay, 'directions_changed', function() {
+		computeTotalDistance(directionsDisplay.directions);
+	});
+	
+	calcRoute();
 
 }
 
 function calcRoute() {
+	var selectedMode = document.getElementById('mode').value;
 	var start = document.getElementById('start').value;
 	var end = document.getElementById('end').value;
+	var waypts = [];
+	
+	var checkboxArray = document.getElementById('waypoints');
+	  for (var i = 0; i < checkboxArray.length; i++) {
+	    if (checkboxArray.options[i].selected == true) {
+	      waypts.push({
+	          location:checkboxArray[i].value,
+	          stopover:true});
+	    }
+	  }
+	
 	var request = {
 		origin : start,
 		destination : end,
-		travelMode : google.maps.TravelMode.DRIVING
+		waypoints: waypts,
+		optimizeWaypoints: true,
+		travelMode: google.maps.TravelMode[selectedMode]
 	};
+	
 	directionsService.route(request, function(response, status) {
 		if (status == google.maps.DirectionsStatus.OK) {
 			directionsDisplay.setDirections(response);
-		}
+			 var route = response.routes[0];
+		      var summaryPanel = document.getElementById('directions_panel');
+		      summaryPanel.innerHTML = '';
+		      // For each route, display summary information.
+		      for (var i = 0; i < route.legs.length; i++) {
+		    	  var routeSegment = i + 1;
+		    	  summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment + '</b><br>';
+		    	  summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+		    	  summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+		    	  summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+		      }
+			}
 	});
 }
 
@@ -57,5 +89,15 @@ function codeAddress() {
 		}
 	});
 }
+
+function computeTotalDistance(result) {
+	  var total = 0;
+	  var myroute = result.routes[0];
+	  for (var i = 0; i < myroute.legs.length; i++) {
+	    total += myroute.legs[i].distance.value;
+	  }
+	  total = total / 1000.
+	  document.getElementById('total').innerHTML = total + ' km';
+	}
 
 google.maps.event.addDomListener(window, 'load', initialize);
